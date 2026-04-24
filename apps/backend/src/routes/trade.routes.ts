c3b21e4f797b0cb821ab1dc4b3877975fb9c5c7c
Amarnath <amarnathreddy841@gmail.com>
2026-04-24 02:51:21 +0000

    add asynchandler to routes

--- apps/backend/src/routes/trade.routes.ts
+++ apps/backend/src/routes/trade.routes.ts
@@ -3,11 +3,12 @@ import { authMiddleware } from "../middlewares/auth";
 import { validate } from "../middlewares/validate";
 import { closeTrade, openTrade } from "../controllers/trade.controller";
 import { closeTradeSchema, openTradeSchema } from "../schemas/trade.schema";
+import { asyncHandler } from "../utils/asyncHandler";
 
 const router: Router = Router();
 
-router.post("/open", authMiddleware, validate(openTradeSchema), openTrade);
+router.post("/open", authMiddleware, validate(openTradeSchema), asyncHandler(openTrade));
 
-router.post("/close", authMiddleware, validate(closeTradeSchema), closeTrade);
+router.post("/close", authMiddleware, validate(closeTradeSchema), asyncHandler(closeTrade));
 
 export const tradeRoutes =  router;
