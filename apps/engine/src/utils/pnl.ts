ba821205f9232827286fd1410d9e5510649b39a1
Amarnath <amarnathreddy841@gmail.com>
2026-04-25 04:14:07 +0000

    Corected logic

--- apps/engine/src/utils/pnl.ts
+++ apps/engine/src/utils/pnl.ts
@@ -1,11 +1,12 @@
-export const calculatePnl = (trade: any, price: number) {
+export const calculatePnl = (trade: any, price: number) => {
     const entryPrice = Number(trade.entryPrice);
     const leverage = Number(trade.leverage);
     const positionSize = Number(trade.positionSize);
 
+    const quantity = positionSize / entryPrice;
     if (trade.side === "long") {
-        return (price - entryPrice) * positionSize * leverage;
+        return (price - entryPrice) * quantity * leverage;
     } else {
-        return (entryPrice - price) * positionSize * leverage;
+        return (entryPrice - price) * quantity * leverage;
     }
 }
