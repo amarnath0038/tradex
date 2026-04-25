96fb4845ec298d0e4e30e53e2c00bb9d872aef5e
Amarnath <amarnathreddy841@gmail.com>
2026-04-25 00:30:44 +0000

    Implement priceloop

---
+++ apps/engine/src/services/priceloop.ts
@@ -0,0 +1,40 @@
+import { db } from "@repo/db";
+import { getPrice, updatePrices } from "../priceStore";
+import { isValidAsset } from "../utils/isValidAsset";
+import { shouldLiquidate } from "../utils/liquidation";
+import { handleCloseTrade } from "../handlers/closeTrade";
+
+export const startPriceloop =  async () => {
+    while (true) {
+        updatePrices();
+
+        console.log("BTC:", getPrice("BTC"));
+
+        const openTrades = await db.query.trades.findMany({
+            where: (t, { eq }) => eq(t.status, "OPEN")
+        });
+
+        for (const trade of openTrades) {
+            if (!isValidAsset(trade.asset)) {
+                console.log("Invalid asset", trade.asset);
+                continue;
+            }
+            const currentPrice = getPrice(trade.asset);
+
+            if (shouldLiquidate(trade, currentPrice)) {
+                console.log("Liquidating:", {
+                    tradeId: trade.id,
+                    asset: trade.asset,
+                    currentPrice
+                });
+
+                await handleCloseTrade({
+                    userId: trade.userId,
+                    tradeId: trade.id,
+                    reason: "liquidation"
+                });
+            }
+        }
+        await new Promise((res) => setTimeout(res, 2000)) // sleep
+    }
+}
