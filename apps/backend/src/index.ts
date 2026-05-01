2aa61c93d66fe4f479761d150ff2b2e5b2092cdb
Amarnath <amarnathreddy841@gmail.com>
2026-05-01 14:52:12 +0000

    Initialise response listener

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,28 @@
 import dotenv from "dotenv";
 import app from "./app";
+import { testDb } from "./utils/testDb";
+import { initResponseListener } from "./services/responseManager";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+// const PORT = Number(process.env.PORT) || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+// app.listen(PORT,() => {
+//   console.log(`Server running on ${PORT}`);
+// });
+
+async function start() {
+  console.log("Backend starting");
+
+  //await testDb();
+  await initResponseListener();
+
+  const PORT = Number(process.env.PORT) || 3001;
+
+  app.listen(PORT,() => {
+    console.log(`Server running on ${PORT}`);
+  });
+
+}
+
+start();
