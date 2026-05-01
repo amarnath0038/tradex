a7f1ffb5aebe06231bc5d7050d4b9af5919bde37
Amarnath <amarnathreddy841@gmail.com>
2026-05-01 14:45:40 +0000

    Added trade response key

--- packages/redis/src/keys.ts
+++ packages/redis/src/keys.ts
@@ -1,5 +1,6 @@
 export const REDIS_KEYS = {
     MARKET_LAST_UPADATE: 'market:lastUpdate',
     TRADES_STREAM: 'trades:stream',  
-    PRICE_CHANNEL: 'channel:prices'
+    PRICE_CHANNEL: 'channel:prices',
+    TRADE_RESPONSES: 'trade:responses'
 }
