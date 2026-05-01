db3779376368f9af09fde3a8298051ad8e0de226
Amarnath <amarnathreddy841@gmail.com>
2026-05-01 14:44:50 +0000

    Added req response handling

--- apps/backend/src/controllers/trade.controller.ts
+++ apps/backend/src/controllers/trade.controller.ts
@@ -1,7 +1,9 @@
+import crypto from "crypto";
 import { Request, Response } from "express";
-import  { stream } from "@repo/redis"
+import  { REDIS_KEYS, stream } from "@repo/redis"
 import { toStreamArgs } from "../utils/streamArgs";
 import { isMarketReady } from "../utils/isMarketReady";
+import { waitForResponse } from "../services/responseManager";
 
 //open trade
 export const openTrade = async (req: Request, res: Response) => {
@@ -19,45 +21,77 @@ export const openTrade = async (req: Request, res: Response) => {
   
   const { asset, side, leverage, positionSize } = req.body;
 
+  if (!asset || !side || !positionSize) { 
+    return res.status(400).json({ error: "missing required fields" }); 
+  }
+
   if (!leverage || leverage <= 0) {
     return res.status(400).json({ error: "invalid leverage" });
   }
 
-  //send to redis stream
   
-  await stream.xadd("stream:app:info", "*", ...toStreamArgs({
+  const requestId = crypto.randomUUID();
+  console.log("Sending trade event", requestId)
+
+  //const responsePromise = await waitForResponse(requestId);
+  //send to redis stream
+  await stream.xadd(REDIS_KEYS.TRADES_STREAM, "*", ...toStreamArgs({
+    payload: JSON.stringify({
       type: "OPEN_TRADE",
+      requestId,
       userId,
       asset,
       side,
       leverage,
       positionSize
     })
+    })
   )
-  return res.json({
-    message: "message sent to engine",
-  });
+
+  try {
+    const result = await waitForResponse(requestId);
+    return res.json(result);
+  } catch (err) {
+    console.log("Open trade  failed", err)
+    return res.status(500).json({ error: "Failed to get response from engine" });
+  }
+ 
 };
 
 
 //close trade
 export const closeTrade = async (req: Request, res: Response) => {
   const userId = req.userId;
-
   if (!userId) {
     return res.status(401).json({ error: "unauthorized" });
   }
 
   const { tradeId } = req.body;
+  if (!tradeId) { 
+    return res.status(400).json({ error: "tradeId required" }); 
+  }
 
-  await stream.xadd("stream:app:info", "*", ...toStreamArgs({
+  const requestId = crypto.randomUUID();
+  //const responsePromise = waitForResponse(requestId); 
+  await stream.xadd(REDIS_KEYS.TRADES_STREAM, "*", ...toStreamArgs({
+    payload: JSON.stringify({
       type: "CLOSE_TRADE",
+      requestId,
       userId,
       tradeId
     })
+    })
   );
 
-  return res.json({
-    message: "close trade request sent to engine"
-  });
+  try {
+    const result = await waitForResponse(requestId);
+    return res.json({
+      data: result,
+      status: result.status === "SUCCESS"
+    });
+  } catch (err) {
+    console.log("Close trade failed", err)
+    return res.status(500).json({ error: "Failed to get response from engine" });
+  }
+
 };
