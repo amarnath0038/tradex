e3d8f277e4a5ecaab5ccef787ae44845f1637013
Amarnath <amarnathreddy841@gmail.com>
2026-05-01 14:48:10 +0000

    Add traderesponse type

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,3 +3,12 @@ export type  RedisStreamMessage = [id: string, fields: string[]];
 export type RedisStream = [stream: string, messages: RedisStreamMessage[]];
 
 export type XReadResponse = RedisStream[] | null;
+
+export type TradeResponse = {
+    requestId: string;
+    status: "SUCCESS" | "ERROR";
+    type?: string;
+    message?: string;
+    pnl?: number;
+    tradeStatus?: "OPEN" | "CLOSED" | "LIQUIDATED";
+}
