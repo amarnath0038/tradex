85f571e7ba7439011cba9919cda16ae98309c30d
Amarnath <amarnathreddy841@gmail.com>
2026-05-05 13:30:19 +0000

    start market data stream from entrypoint

--- apps/poller/src/index.ts
+++ apps/poller/src/index.ts
@@ -1,45 +1,18 @@
-import { pub, REDIS_KEYS } from "@repo/redis";
-import axios from "axios";
+import { BInanceMarketDataStream } from "./marketData/binanceWs";
 
-const ASSETS = ["BTC", "ETH", "SOL"];
-const url = process.env.BINANCE_URL || "https://api.binance.com/api/v3/ticker/price";
+const marketData = new BInanceMarketDataStream();
 
-const fetchPrices = async () => {
+const shutdown = (signal: string) => {
+    console.log(`Received ${signal}. Shutting down gracefully`);
     try {
-        const res = await axios.get(url);
-
-        const data = res.data;
-        const prices: Record<string, number> = {};
-
-        for (const item of data) {
-            if (item.symbol === "BTCUSDT") prices.BTC = Number(item.price); 
-            if (item.symbol === "ETHUSDT") prices.ETH = Number(item.price); 
-            if (item.symbol === "SOLUSDT") prices.SOL = Number(item.price); 
-        }
-
-        return prices;
-
+        marketData.stop();
     } catch (err) {
-        console.log("Error fetching prices");
-        return null;
+        console.error("Error stopping market data service")
     }
+    process.exit(0);
 }
 
-async function start() {
-    console.log("Poller started");
-
-    setInterval( async () => {
-        const prices = await fetchPrices();
-        if (!prices) return;
-
-        for(const asset of ASSETS) {
-            const price = prices[asset];
-            if (!price) continue;
-
-            await pub.publish(REDIS_KEYS.PRICE_CHANNEL, JSON.stringify({asset, price}))
-            console.log(asset, price);
-        }
-    }, 2000)
-}
-
-start();
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+ 
+marketData.start(); 
