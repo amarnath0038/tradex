bf995dab07109d9091e7b964675c14d912d3bf56
Amarnath <amarnathreddy841@gmail.com>
2026-05-10 09:24:51 +0000

    fix typo

--- apps/backend/src/utils/isMarketReady.ts
+++ apps/backend/src/utils/isMarketReady.ts
@@ -1,7 +1,7 @@
 import { REDIS_KEYS, state } from "@repo/redis"
 
 export const isMarketReady = async (): Promise<Boolean> => {
-    const lastUpdate = await state.get(REDIS_KEYS.MARKET_LAST_UPADATE);
+    const lastUpdate = await state.get(REDIS_KEYS.MARKET_LAST_UPDATE);
 
     if (!lastUpdate) return false;
 
