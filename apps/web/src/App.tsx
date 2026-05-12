ac814fa3b871d558fe7653e83efb7bff2a952331
Amarnath <amarnathreddy841@gmail.com>
2026-05-12 06:49:31 +0000

    Replace Nextjs frontend with vite react app

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-blue-700 p-4 text-5xl'>
+      Lets go and build cool stuff
+    </div>
+  );
+};
+
+export default App;
